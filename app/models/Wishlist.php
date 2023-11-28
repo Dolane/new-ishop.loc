e5252b22b1d536c56933a811d7a55fa0f3bed807
Dolane <s.chepara123@gmail.com>
2023-11-28 08:37:01 +0200

    31

--- app/models/Wishlist.php
+++ app/models/Wishlist.php
@@ -45,4 +45,31 @@ class Wishlist extends AppModel
         return [];
     }
 
+    public function get_wishlist_products($lang): array
+    {
+        $wishlist = self::get_wishlist_ids();
+        if ($wishlist) {
+            $wishlist = implode(',', $wishlist);
+            return R::getAll("SELECT p.*, pd.* FROM product p JOIN product_description pd on p.id = pd.product_id WHERE p.status = 1 AND p.id IN ($wishlist) AND pd.language_id = ? LIMIT 6", [$lang['id']]);
+        }
+        return [];
+    }
+
+    public function delete_from_wishlist($id): bool
+    {
+        $wishlist = self::get_wishlist_ids();
+        $key = array_search($id, $wishlist);
+        if (false !== $key) {
+            unset($wishlist[$key]);
+            if ($wishlist) {
+                $wishlist = implode(',', $wishlist);
+                setcookie('wishlist', $wishlist, time() + 3600*24*7*30, '/');
+            } else {
+                setcookie('wishlist', '', time()-3600, '/');
+            }
+            return true;
+        }
+        return false;
+    }
+
 }
