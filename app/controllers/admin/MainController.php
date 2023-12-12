87d64210772cb02b7a997a3bad1460e2ecb0596c
Dolane <s.chepara123@gmail.com>
2023-12-12 06:59:06 +0200

    (Admin part) 2

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -9,7 +9,9 @@ class MainController extends AppController
 
     public function indexAction()
     {
-
+        $title = 'Головна сторінка';
+        $this->setMeta('Адмінка :: Головна сторінка');
+        $this->set(compact('title'));
     }
 
 }
